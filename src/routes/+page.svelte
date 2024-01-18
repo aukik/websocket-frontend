2b653252fcbcf05357425031cfaf085b62830a62
aukik <aukikaurnabx@gmail.com>
2024-01-18 17:55:23 +0600

    new ui added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Alert } from 'flowbite-svelte';
     import { onMount, onDestroy } from 'svelte';
     // import io from 'socket.io-client';
 
@@ -19,6 +20,8 @@
     let profileLink = '';
     let orderAmount = '';
     let maxPerPost = '';
+
+    let scrapperButtonDisabled = false;
 
     // onMount(() => {
     //     socket = io('localhost:5000');
@@ -61,7 +64,7 @@
             const response = await fetch(import.meta.env.VITE_URL+'/start_new_posts_checker', {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Bearer drdre'
+                    'Authorization': `Bearer ${sessionStorage.getItem('password')}` 
                 },
             });
 
@@ -72,6 +75,7 @@
             } else {
                 console.error('Failed to start new posts checker.');
             }
+            showMessage();
         } catch (error) {
             console.error('Error:', error);
         }
@@ -83,7 +87,7 @@
             const response = await fetch(import.meta.env.VITE_URL+'/checkForState', {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Bearer drdre'
+                    'Authorization': `Bearer ${sessionStorage.getItem('password')}` 
                 },
             });
             if (response.ok) {
@@ -103,7 +107,7 @@
 
     const startPolling = () => {
         checkForState(); // Initial call
-        setInterval(checkForState, 30000); // Poll every 30 seconds
+        setInterval(checkForState, 10000); // Poll every 5 seconds
     };
 
 
@@ -113,11 +117,16 @@
 
 
     const endCurrentProcess = async () => {
+        if(scrapperButtonDisabled) {
+            currentState="Please try again after a while!"
+            showMessage();
+            return;
+        }
         try {
             const response = await fetch(import.meta.env.VITE_URL+'/end', {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Bearer drdre'
+                    'Authorization': `Bearer ${sessionStorage.getItem('password')}` 
                 },
             });
 
@@ -128,6 +137,7 @@
             } else {
                 console.error('Failed to End the process.');
             }
+            showMessage();
         } catch (error) {
             console.error('Error:', error);
         }
@@ -139,7 +149,7 @@
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer drdre'
+                    'Authorization': `Bearer ${sessionStorage.getItem('password')}` 
                 },
                 body: JSON.stringify({
                     order_id: orderId,
@@ -158,107 +168,216 @@
                 console.error('Failed to make POST request.');
                 currentState = "Failed to add user!"
             }
+            showMessage();
         } catch (error) {
             console.error('Error:', error);
         }
     }
 
     // New form fields
-    let instances = '';
+    let instances = 1;
 
     const startInstances = async () => {
         try {
+            if(instances==0) {
+                currentState="Number of instances must be greater than zero!"
+                showMessage();
+                return;
+            }
+            if(scrapperButtonDisabled || isInstancesRunning) {
+                currentState="Scrapper is already running!"
+                showMessage();
+                return;
+            }
+            scrapperButtonDisabled = true;
             currentState = `Starting ${instances} instances...`
             const response = await fetch(import.meta.env.VITE_URL+`/start?instances=${instances}`, {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Bearer drdre'
+                    'Authorization': `Bearer ${sessionStorage.getItem('password')}` 
                 },
             });
 
             if (response.ok) {
                 console.log(`Started ${instances} instances successfully.`);
                 currentState = `Started ${instances} instances successfully!`
+               
             } else {
                 console.error(`Failed to start ${instances} instances.`);
                 currentState = `Failed to start ${instances} instances!`
             }
+            showMessage();
+            scrapperButtonDisabled = false;
         } catch (error) {
             console.error('Error:', error);
         }
+    }
+
+    
+
+    let showAlert = false;
+    let password = null
+    function showMessage() {
+        showAlert = true;
+        setTimeout(() => {
+            showAlert = false;
+        }, 10000);
+
     }
 </script>
 
 <style>
     @import './css/styles.css';
 </style>
+{#if showAlert}
+<div role="alert" class="alert alert-info w-fit alert-info shadow-lg fixed right-4 top-4 right px-8">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path  strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <div>
+      <h3 class="font-bold text-white">{currentState}</h3>
+   
+    </div>
+
+  </div>
+  {/if}
+
+
+    
+
+
+  
 <div  style="max-height: 90vh; , overflowY: auto">
-<div class="w-full flex flex-row justify-center my-8">
-    <div class="flex flex-col justify-center">
-        <p class="text-2xl font-bold self-center">Welcome to Instagram Scrapper</p>
+<div class="w-full  flex flex-row justify-center my-8">
+    <div class="flex flex-col  justify-center">
+        <p class="text-2xl text-white font-bold self-center">Welcome to Instagram Scrapper</p>
         
     </div>
 </div>
 
-<div class="w-full h-full flex flex-row justify-center my-8">
+<!-- <div class="w-full h-full flex flex-row justify-center my-8">
     <div class="flex flex-col justify-center">
         <p class="text-2xl font-bold self-center">Current State</p>
         <p class="text-2xl font-bold self-center" style="color: red;">{currentState}</p>
     </div>
-</div>
+</div> -->
 
-<div class="w-full h-full flex flex-row justify-center my-8">
-    {#if isInstancesRunning}
-        <div class="flex flex-col justify-center ">
-            <p class="text-2xl font-bold self-center">{numberOfInstances} instances Running!</p>
+
+
+
+<div class="flex flex-row justify-around">
+    <div class="w-1/2 px-16">
+        <div>
+            <div class="flex flex-col items-center my-8">
+            
+                <div class="w-full h-full flex flex-row justify-center my-8">
+                    {#if isInstancesRunning}
+                        <div class="flex flex-col justify-center ">
+                            <p class="text-2xl font-bold self-center">Current State: {numberOfInstances} instances Running!</p>
+                        </div>
+                    {:else if isNewUserAdding}
+                        <div class="flex flex-col justify-center ">
+                            <p class="text-2xl font-bold self-center">Current State: New User is being added!</p>
+                        </div>
+                    {:else if isPostCheckingOn}
+                        <div class="flex flex-col justify-center ">
+                            <p class="text-2xl font-bold self-center">Current State: Post checker is on!</p>
+                        </div>
+                    {:else}
+                        <div class="flex flex-col justify-center ">
+                            <p class="text-2xl font-bold self-center">Current State: No process is Running!</p>
+                        </div>
+                    {/if}
+                </div>
+    
+            </div>
+            <div class="flex flex-col items-center my-8 bg-slate-100 m-8 p-4 rounded-lg">
+                <p class="text-lg text-black font-bold  my-2">Are multi-instances running: <span class="font-normal">{isInstancesRunning?"Yes":"No"}</span></p>
+                <p class="text-lg text-black font-bold  my-2">Number of instances running: <span class="font-normal">{numberOfInstances}</span></p>
+                <p class="text-lg text-black font-bold  my-2">Is New Post Checker Running: <span class="font-normal">{isPostCheckingOn?"Yes":"No"}</span></p>
+                <p class="text-lg text-black font-bold  my-2">Is Premium User Insertion Running: <span class="font-normal">{isNewUserAdding?"Yes":"No"}</span></p>
+            </div>
+            
+            
         </div>
-    {:else if isNewUserAdding}
-        <div class="flex flex-col justify-center ">
-            <p class="text-2xl font-bold self-center">New User is being added!</p>
+
+        <div role="alert" class="alert alert-success flex flex-col items-center alert-info shadow-lg right ">
+           
+            <div>
+              <h3 class="font-bold text-white">Enter Your Password:</h3>
+                <input type="password" id="password" bind:value={password}  class="bg-slate-50" />
+                <div class="btn btn-accent my-2" on:click={()=>{sessionStorage.setItem('password',password)}}>Submit</div>
+           
+            </div>
+        
+          </div>
+
+        
+        
+        
+        
+    </div>
+
+    <div class="w-1/2 px-16">
+
+        <div class=" bg-slate-100 rounded-lg flex flex-col items-center m-8 p-4">
+            <h1 class="text-xl text-black font-bold self-center my-2">Run Scrapper</h1>
+            <div >
+                <label class="text-black " for="instances">Number of Instances:</label>
+                <input class=" bg-slate-50" type="number" id="instances" bind:value={instances}  />
+                <div class="flex flex-row justify-around my-4">
+                    {#if scrapperButtonDisabled==false}
+                        <div class="btn btn-success bg-white border border-gray-800 mx-2" on:click={startInstances}>Start Instances</div>
+                    {:else}
+                        <div class="loading loading-spinner loading-sm"></div>
+                    {/if}
+                    <div class="btn btn-error bg-black border border-gray-300 text-white mx-2" on:click={endCurrentProcess}>End Scrapping</div>
+                    
+                    <!-- <button on:click={showMessage}>gg</button> -->
+                </div>
+            </div>
+            
+            
+
         </div>
-    {:else if isPostCheckingOn}
-        <div class="flex flex-col justify-center ">
-            <p class="text-2xl font-bold self-center">Post checker is on!</p>
+        
+        
+        <div class=" flex flex-col items-center my-8 bg-slate-100 m-8 p-4 rounded-lg">
+            <h1 class="text-xl text-black font-bold self-center my-2">Add New Premium User</h1>
+            <div class="my-2">
+                <label class="text-black" for="orderId">Order ID:</label>
+                <input  type="text" id="orderId" bind:value={orderId} class="bg-slate-50" />
+            </div>
+            
+            <div class="my-2">
+                <label class="text-black" for="profileLink">Profile Link:</label>
+                <input type="text" id="profileLink" bind:value={profileLink} class="bg-slate-50" />
+            </div>
+            
+            <div class="my-2">
+                <label class="text-black" for="orderAmount">Order Amount:</label>
+                <input type="text" id="orderAmount" bind:value={orderAmount} class="bg-slate-50" />
+            </div>
+            
+            
+            <div class="my-2">
+                <label class="text-black" for="maxPerPost">Max Per Post:</label>
+                <input type="text" id="maxPerPost" bind:value={maxPerPost} class="bg-slate-50" />
+            </div>
+            
+        
+            <div class="btn btn-primary my-2" on:click={submitPostRequest}>Add New User</div>
         </div>
-    {:else}
-        <div class="flex flex-col justify-center ">
-            <p class="text-2xl font-bold self-center">No process is Running!</p>
-        </div>
-    {/if}
+        
+        
+        <!-- New form components -->
+        
+        <!-- <div class="flex flex-row justify-center my-4">
+            <div class="btn btn-primary bg-blue-500 text-white border border-black p-2 rounded-lg my-4" on:click={startNewPostsChecker}>Check for new post</div>
+        </div> -->
+        
+    </div>
+
+
 </div>
 
 
-
-<div class="w-full h-full flex flex-row justify-center my-8">
-    <button class="bg-blue-500 text-white border border-black p-2 rounded-lg my-4" on:click={startNewPostsChecker}>Check Users Posts</button>
-</div>
-
-<div class="w-full h-full flex flex-col justify-center my-8 form-container">
-    <h1 class="text-2xl font-bold self-center my-2">Add New Premium User</h1>
-    <label for="orderId">Order ID:</label>
-    <input type="text" id="orderId" bind:value={orderId} class="form-group" />
-
-    <label for="profileLink">Profile Link:</label>
-    <input type="text" id="profileLink" bind:value={profileLink} class="form-group" />
-
-    <label for="orderAmount">Order Amount:</label>
-    <input type="text" id="orderAmount" bind:value={orderAmount} class="form-group" />
-
-    <label for="maxPerPost">Max Per Post:</label>
-    <input type="text" id="maxPerPost" bind:value={maxPerPost} class="form-group" />
-
-    <button on:click={submitPostRequest}>Add New User</button>
-</div>
-
-<!-- New form components -->
-<div class="w-full h-full flex flex-col justify-center my-8 new-form-container">
-    <h1 class="text-2xl font-bold self-center my-2">Run Scrapper</h1>
-    <label for="instances">Number of Instances:</label>
-    <input type="text" id="instances" bind:value={instances} class="form-group" />
-
-    <button on:click={startInstances}>Start Instances</button>
-</div>
-<div class="w-full h-full flex flex-row justify-center my-8">
-    <button style="background-color: red;" class="bg-blue-500 text-white border border-black p-2 rounded-lg my-4" on:click={endCurrentProcess}>End Scrapping</button>
-</div>
 </div>
